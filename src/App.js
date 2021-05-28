e7c95d921d55c44325fddab1f7188bbe4d4b78e4
Josh Burgess <burgessj247@gmail.com>
2021-05-28 15:46:20 -0400

    gh-pages fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import "./styles/App.scss";
-import React from "react";
-import { Switch, Route, Redirect, useLocation } from "react-router-dom";
-import ShopProvider from "./context/shopProvider";
-import { Client as Styletron } from "styletron-engine-atomic";
-import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
-import Rescroll from "./functions/Top";
-import { AnimatePresence } from "framer-motion";
+import './styles/App.scss';
+import React from 'react';
+import { Switch, Route, Redirect, useLocation } from 'react-router-dom';
+import ShopProvider from './context/shopProvider';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider, DebugEngine } from 'styletron-react';
+import Rescroll from './functions/Top';
+import { AnimatePresence } from 'framer-motion';
 
 //componenets
 // import Nav from "./components/Nav";
-import Home from "./pages/Home";
-import AgeRestriction from "./pages/RestrictionPage";
-import Products from "./pages/Products";
-import Cart from "./components/Cart";
+import Home from './pages/Home';
+import AgeRestriction from './pages/RestrictionPage';
+import Products from './pages/Products';
+import Cart from './components/Cart';
 // import ProgressBar from "./components/ProgressBar";
-import PageNotFound from "./pages/404";
+import PageNotFound from './pages/404';
 
 const debug =
-  process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
+  process.env.NODE_ENV === 'production' ? void 0 : new DebugEngine();
 const engine = new Styletron();
 
 //Use to be a class for the Progress Bar, need to figure out how to do it without class
@@ -25,7 +25,7 @@ function App() {
   const location = useLocation();
 
   return (
-    <section className="App">
+    <section className='App'>
       <ShopProvider>
         <StyletronProvider value={engine} debug={debug} debugAfterHydration>
           {/* <Nav /> */}
@@ -35,11 +35,11 @@ function App() {
           <Rescroll />
           <AnimatePresence exitBeforeEnter>
             <Switch location={location} key={location.key}>
-              <Route path="/" exact component={AgeRestriction} />
-              <Route path="/home" exact component={Home} />
-              <Route path="/products" exact component={Products} />
-              <Route path="*" component={PageNotFound} />
-              <Redirect to="/404" />
+              <Route path='/' exact component={AgeRestriction} />
+              <Route path='/home' exact component={Home} />
+              <Route path='/products' exact component={Products} />
+              {/* <Route path="*" component={PageNotFound} />
+              <Redirect to="/404" />  for gh-pages*/}
             </Switch>
           </AnimatePresence>
         </StyletronProvider>
